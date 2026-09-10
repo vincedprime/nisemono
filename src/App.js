2e86167f619eb738f2142bd81a96968963ac7315
vincedprime <vinayparida099@gmail.com>
2026-09-11 02:40:49 +0530

    Support GitHub Pages project path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import WiFiInvoice from "./pages/WiFiInvoice";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/udemy-certificate" element={<UdemyCertificate />} />
